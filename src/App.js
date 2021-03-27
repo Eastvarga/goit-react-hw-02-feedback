cabf3be44f285a353a976ac76abcad2bd5f4ecc2
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-27 20:41:43 +0200

    main component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import FeadbackContent from "./components/FeadbackContent";
+
+function App() {
+  return (
+    <div className="App">
+      <FeadbackContent />
+    </div>
+  );
+}
+
+export default App;
